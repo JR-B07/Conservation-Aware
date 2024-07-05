3dff460217b610335270aecbaf690dbc1ba01e26
RicardoBeCo <125924702+JR-B07@users.noreply.github.com>
2024-07-05 17:58:31 -0600

    vistas

--- app.py
+++ app.py
@@ -1,87 +1,112 @@
+from flask import *
 from flask import Flask, flash, redirect, request, render_template, url_for
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'conservationaware'
-app.secret_key = 'your_secret_key'  
+app.secret_key = 'your_secret_key'
 mysql = MySQL(app)
+
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+
+@app.route('/sobre_nosotros')
+def sobre_nosotros():
+    return render_template('sobre_nosotros.html')
+
+
+@app.route('/animales')
+def animales():
     try:
         cursor = mysql.connection.cursor()
-        cursor.execute('SELECT * FROM albums')
-        consultaA = cursor.fetchall()
-        return render_template('index.html', albums=consultaA)
+        cursor.execute("SELECT * FROM animals")
+        animales = cursor.fetchall()
+        cursor.close()
+        return render_template('animales.html', animals=animales)
     except Exception as e:
         print(e)
-        return "Error al realizar la consulta", 500
+        flash('Error al obtener los datos de la base de datos', 'error')
+        return redirect(url_for('index'))
 
-@app.route('/guardarAlbum', methods=['POST'])
-def guardarAlbum():
+
+@app.route('/donar')
+def donar():
+    return render_template('donar.html')
+
+
+@app.route('/asociaciones')
+def asociaciones():
+    return render_template('asociaciones.html')
+
+
+@app.route('/guardarAnimal', methods=['POST'])
+def guardarAnimal():
     if request.method == 'POST':
-        FTitulo = request.form['txtTitulo']
-        FArtista = request.form['txtArtista']
-        FAño = request.form['txtAnio']
+        common_name = request.form['common_name']
+        scientific_name = request.form['scientific_name']
+        description = request.form['description']
+        estimated_population = request.form['estimated_population']
+        image_url = request.form['image_url']
         try:
             cursor = mysql.connection.cursor()
-            cursor.execute("INSERT INTO albums (titulo, artista, anio) VALUES (%s, %s, %s)", (FTitulo, FArtista, FAño))
+            cursor.execute(
+                "INSERT INTO animals (common_name, scientific_name, description, estimated_population, image_url) VALUES (%s, %s, %s, %s, %s)",
+                (common_name, scientific_name, description, estimated_population, image_url))
             mysql.connection.commit()
-            flash('Álbum Guardado Correctamente')
-            return redirect(url_for('index'))
+            flash('Animal guardado correctamente')
+            return redirect(url_for('animales'))
         except Exception as e:
             print(e)
             flash('Error al guardar en la base de datos', 'error')
-            return redirect(url_for('index'))
+            return redirect(url_for('animales'))
+
 
 @app.route('/editar/<int:id>')
 def editar(id):
     try:
         cursor = mysql.connection.cursor()
-        cursor.execute('SELECT * FROM albums WHERE id=%s', [id])
-        albumE = cursor.fetchone()
-        return render_template('editar.html', album=albumE)
+        cursor.execute('SELECT * FROM animals WHERE id=%s', [id])
+        animal = cursor.fetchone()
+        return render_template('editar.html', animal=animal)
     except Exception as e:
         print(e)
         return "Error al realizar la consulta", 500
 
-@app.route('/ActualizarAlbum/<int:id>', methods=['POST'])
-def ActualizarAlbum(id):
+
+@app.route('/ActualizarAnimal/<int:id>', methods=['POST'])
+def ActualizarAnimal(id):
     if request.method == 'POST':
         try:
-            Ftitulo = request.form['txtTitulo']
-            Fartista = request.form['txtArtista']
-            Fanio = request.form['txtAnio']
+            common_name = request.form['common_name']
+            scientific_name = request.form['scientific_name']
+            description = request.form['description']
+            estimated_population = request.form['estimated_population']
+            image_url = request.form['image_url']
 
             cursor = mysql.connection.cursor()
-            cursor.execute('UPDATE albums SET titulo=%s, artista=%s, anio=%s WHERE id=%s', (Ftitulo, Fartista, Fanio, id))
+            cursor.execute(
+                'UPDATE animals SET common_name=%s, scientific_name=%s, description=%s, estimated_population=%s, image_url=%s WHERE id=%s',
+                (common_name, scientific_name, description, estimated_population, image_url, id))
             mysql.connection.commit()
-            flash('Álbum editado correctamente')
-            return redirect(url_for('index'))
+            flash('Animal editado correctamente')
+            return redirect(url_for('animales'))
         except Exception as e:
             print(e)
-            flash('Error al actualizar el álbum', 'error')
+            flash('Error al actualizar el animal', 'error')
             return redirect(url_for('editar', id=id))
 
-@app.route('/EliminarAlbum/<int:id>')
-def eliminar(id):
-    try:
-        cursor = mysql.connection.cursor()
-        cursor.execute('DELETE FROM albums WHERE id=%s', [id])
-        mysql.connection.commit()
-        flash('Álbum eliminado')
-        return redirect(url_for('index'))
-    except Exception as e:
-        print(e)
-        flash('Error al eliminar álbum', 'error')
-        return redirect(url_for('index'))
 
 @app.errorhandler(404)
-def paginanotfound(e):
-    return 'Revisa tu sintaxis: No encontré nada', 404
+def pagina_no(e):
+    return 'PÁGINA NO ENCONTRADA'
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
